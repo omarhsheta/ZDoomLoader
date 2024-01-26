795fa11cb457249b2427b763a6cef1c024eca5ad
Omar <osh18@uclive.ac.nz>
2024-01-26 21:48:20 +1300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ZDoomLoader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            engine_label = new Label();
+            SuspendLayout();
+            // 
+            // engine_label
+            // 
+            engine_label.AutoSize = true;
+            engine_label.Location = new Point(12, 42);
+            engine_label.Name = "engine_label";
+            engine_label.Size = new Size(79, 15);
+            engine_label.TabIndex = 0;
+            engine_label.Text = "Doom Engine";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(engine_label);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label engine_label;
+    }
+}
