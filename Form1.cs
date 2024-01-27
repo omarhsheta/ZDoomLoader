e942b4fa5f516ad8d262a01434987b2326267f86
Omar <osh18@uclive.ac.nz>
2024-01-27 22:19:46 +1300

    PK3 files are supported now
    Now can run PK3 files, but no way to check all mods or unselect them.

--- Form1.cs
+++ Form1.cs
@@ -51,6 +51,7 @@ namespace ZDoomLoader
                         args += item.ToString() + ' ';
                     }
                     System.Diagnostics.Process.Start(engine_input.Text, args);
+                    Console.WriteLine($"{engine_input.Text} {args}");
                 } else
                 {
                     System.Diagnostics.Process.Start(engine_input.Text);
@@ -71,23 +72,25 @@ namespace ZDoomLoader
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
                 openFileDialog.InitialDirectory = "c:\\";
-                openFileDialog.Filter = "PWAD Files (*.wad)|*.wad";
+                openFileDialog.Filter = "DooM mod files (*.wad or *.pk3 or *.ipk3)|*.wad;*.pk3;*.ipk3";
                 openFileDialog.FilterIndex = 2;
                 openFileDialog.RestoreDirectory = true;
+                openFileDialog.Multiselect = true;
 
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    //Get the path of specified file
-                    filePath = openFileDialog.FileName;
-                    var fileName = filePath.Split("\\");
-                    string[] iwadFiles = ["doom.wad", "doom2.wad", "plutonia.wad", "tnt.wad"];
-                    if (iwadFiles.Contains(fileName[fileName.Length - 1].ToLower()))
+                    foreach (string file in openFileDialog.FileNames)
                     {
-                        MessageBox.Show("You cannot choose an IWAD, such as Doom 1, Doom 2, ...etc", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                    else
-                    {
-                        pwad_list.Items.Add(filePath);
+                        var fileName = file.Split("\\");
+                        string[] iwadFiles = ["doom.wad", "doom2.wad", "plutonia.wad", "tnt.wad"];
+                        if (iwadFiles.Contains(fileName[fileName.Length - 1].ToLower()))
+                        {
+                            MessageBox.Show("You cannot choose an IWAD, such as Doom 1, Doom 2, ...etc", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else
+                        {
+                            pwad_list.Items.Add(file);
+                        }
                     }
                 }
             }
